ba740b7d513ba9d3ed0dc058d78c683bf90bd8f8
Jaime Moncada <jaimemoncada13@gmail.com>
2017-05-08 08:57:23 -0500

    Major restructure of my code and new css styling

--- js/simple-simon.js
+++ js/simple-simon.js
@@ -6,9 +6,10 @@
 $(document).ready(function () {
     "use strict";
 
-    var AiCurrent = [];
+    var sequence = [];
     var round = 0;
     var i = 0;
+    var blinkTimer = 0;
     var active = false;
     var colors = ["yellow", "blue", "red", "green"];
 
@@ -18,36 +19,38 @@ $(document).ready(function () {
         active = true;
     }
 
-    $("#start-game").click("click", toggleStart);
+    // $("#start-game").on("click", toggleStart);
 
     function nextRound() {
         $("#round-lose").hide();
         if (active == true) {
-            AiCurrent.push(colors[randomNumber()]);
-            round = AiCurrent.length;
-            console.log("AI " + AiCurrent);
+            sequence.push(colors[randomNumber()]);
+            round = sequence.length;
+            $("#round").html(round);
+            masterAnimate();
+            console.log("AI " + sequence);
             console.log("<=========>");
         }
 
-        AiCurrent.forEach(function (el, ind) {
-            var idTimer = setTimeout(function () {
-                aniClick("#" +el);
-            }, (1000 * ind));
-
-            console.log(ind);
-            console.log(AiCurrent.length);
-
-        });
+        // sequence.forEach(function (el, ind) {
+        //     var idTimer = setTimeout(function () {
+        //         masterAnimate("#" +el);
+        //     }, (1000 * ind));
+        //     console.log(ind);
+        //     console.log(sequence.length);
+        //
+        // });
 
 
     }
 
+
     function match(idTag) {
-        console.log(AiCurrent[i]);
+        console.log(sequence[i]);
         console.log(idTag);
-        if (AiCurrent[i] == idTag) {
+        if (sequence[i] == idTag) {
             ++i;
-            if (i == AiCurrent.length) {
+            if (i == sequence.length) {
                 console.log("Round Complete");
                 i = 0;
                 nextRound();
@@ -61,40 +64,53 @@ $(document).ready(function () {
 
     //===============================================================================================================================
 
-    function animate(element) {
-        $(element).animate({
-            opacity: .5
+    function masterAnimate() {
+        blinkTimer++;
+        $("#" + sequence[blinkTimer-1]).animate({
+            opacity: .2
         }, 500).animate({
             opacity: 1
-        }, 500)
+        }, 500, function () {
+            if (blinkTimer < sequence.length) {
+                console.log("animating " + sequence[blinkTimer-1]);
+                console.log("Keep going");
+                masterAnimate();
+            }else {
+                console.log("Finished");
+                blinkTimer = 0;
+            }
+        });
+
     }
 
-    function aniClick(element) {
-        $(element).animate({
-
-        }, 500).animate({
-            width: 250,
-            height: 250
-        }, 500)
-    }
 
     $(".nl-simon-boxes").on("click", function () {
-        animate($(this));
+        $(this).animate({
+            opacity: .2
+        }, 500).animate({
+            opacity: 1
+        }, 500);
         match($(this).attr("id"));
     });
 
 
-    $("#start-game").click("click", nextRound);
-
-    // function arClass() {
-    //     $(".nl-simon-boxes").addClass("blink").removeClass("blink");
+    // $("#start-game").click("click", nextRound);
+    //
+    // // function arClass() {
+    // //     $(".nl-simon-boxes").addClass("blink").removeClass("blink");
+    // //     return false;
+    // // }
+    //
+    // $("#start-game").on("click", function () {
+    // //     // $(".nl-simon-boxes").removeClass("blink");
+    // //     // $(".nl-simon-boxes").addClass("blink");
     //     return false;
-    // }
+    // });
 
-    $("#start-game").on("click", function () {
-        // $(".nl-simon-boxes").removeClass("blink");
-        // $(".nl-simon-boxes").addClass("blink");
-        return false;
+
+    $("#start-game").click(function () {
+        toggleStart();
+        nextRound();
     });
 
     function randomNumber() {
